39fa9c2e7f5f4e0d5a5c1ec030dd106f75702f4e
ngthnhan <ngthnhan93@gmail.com>
2016-10-05 05:40:53 +0800

    Change piechart to donut with polyline tagged labels

--- public/js/controller-3219.js
+++ public/js/controller-3219.js
@@ -1,29 +1,60 @@
 d3.tip = require('./vendor/d3-tip');
 
 // Constants
-COMMITS_CULL_THRESHOLD = 20;
-TOOLTIP_ANGLE_THRESHOLD = 0.2;
+COMMITS_CULL_THRESHOLD = 110;
 
 viz.data.getContribution(handleNewContribution);
 
 function handleNewContribution(data) {
   data = filterImportant(data.map(type));
 
-  var g = svg.selectAll('.arc')
+  // Label in the center
+  svg.select('.donut-label')
+    .attr('dy', '.35em')
+    .style('font-size', '32')
+    .style('font-weight', 'bolder')
+    .style('text-anchor', 'middle')
+    .text("Commits");
+      
+  // Slices of the pie/donut
+  svg.select('.slices').selectAll('path.slice')
       .data(pie(data))
-    .enter().append('g')
-      .attr('class', 'arc')
-      .on("mouseover", tip.show)
-      .on("mouseout", tip.hide);
+    .enter()
+    .insert('path')
+    .style('fill', function(d) { return color(d.data.author.id); })
+    .attr('class', 'slice')
+    .attr('d', arc);
 
-  g.append('path')
-      .attr('d', arc)
-      .style('fill', function(d) { return color(d.data.author.id); });
+  // Label text
+  function midAngle(d) {
+      return d.startAngle + (d.endAngle - d.startAngle) / 2;
+  }
 
-  g.append('text')
-      .attr('transform', function(d) { return 'translate(' + labelArc.centroid(d) + ')'; })
+  svg.select('.labels').selectAll('text')
+      .data(pie(data))
+    .enter().append('text')
       .attr('dy', '.35em')
+      .attr('transform', function(d) { 
+          // Decide to render on the left or right side
+          var pos = outerArc.centroid(d);
+          pos[0] = radius * (midAngle(d) < Math.PI ? 1 : -1);
+          return 'translate(' + pos + ')';
+      })
+      .style('text-anchor', function(d) {
+          return midAngle(d) < Math.PI ? "start" : "end";
+      })
       .text(formatText);
+
+  // Polyline from the slice to text
+  svg.select('.lines').selectAll('polyline')
+      .data(pie(data))
+    .enter()
+      .append('polyline')
+      .attr('points', function(d) {
+        var pos = outerArc.centroid(d);
+        pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
+        return [arc.centroid(d), outerArc.centroid(d), pos];
+    });
 }
 
 function formatText(d) {
@@ -32,7 +63,7 @@ function formatText(d) {
     angle = d.endAngle - d.startAngle;
 
     // Only display inline if the arc is large enough
-    return angle > TOOLTIP_ANGLE_THRESHOLD ? d.data.author.login + ': ' + d.data.total : "";
+    return d.data.author.login + ': ' + d.data.total;
 }
 
 function filterImportant(data) {
@@ -69,12 +100,12 @@ var radius = Math.min(width, height) / 2;
 var color = d3.scale.category20();
 
 var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
+    .outerRadius(radius * 0.7)
+    .innerRadius(radius * 0.3);
 
-var labelArc = d3.svg.arc()
-    .outerRadius(radius - 40)
-    .innerRadius(radius - 40);
+var outerArc = d3.svg.arc()
+    .outerRadius(radius * 0.9)
+    .innerRadius(radius * 0.9)
 
 var pie = d3.layout.pie()
     .sort(null)
@@ -86,15 +117,16 @@ var svg = d3.select('#container').append('svg')
   .append('g')
     .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-var tip = d3.tip()
-      .attr('class', 'd3-tip')
-      .offset(function() {
-          return [this.getBBox().height / 2, 0]
-      })
-      .html(function(d) {
-        return d.data.author.login + ": " + d.data.total;
-      });
-svg.call(tip);
+svg.append('g')
+    .attr('class', 'slices');
+svg.append('g')
+    .attr('class', 'labels');
+svg.append('g')
+    .attr('class', 'lines');
+
+svg.append('g')
+  .append('text')
+    .attr('class', 'donut-label');
 
 function type(d) {
   d.total = +d.total;
